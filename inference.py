80db3b23f8dac3af1d66b388c673e251e66be233
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-05-16 00:12:12 +0900

    structure 2

--- inference.py
+++ inference.py
@@ -143,11 +143,11 @@ def get_args_parser():
     parser.add_argument('--text', default='', type=str, help='Context of word')
     parser.add_argument('--abbrv', default=False, type=bool, help='Turn on or off to change abbreviation')
      
-def main():
-    synonym_recommend = synonym_recommendation(model="llama2", targetWord="CT", sentence=True, cntxt_len=2, text="The radiological evaluation of patients with acute spinal trauma has always been a challenging problem. Multiple radiological procedures are often necessary for \
-                                               complete evaluation of the extent of spinal injury. CT provides an ideal modality whereby accurate assessment of displacement of bony fragments as well as associated spinal cord and nerve root injury can easily be performed, \
-                                               eliminating the need for difficult radiological procedures.")
-    print(synonym_recommend.post_processing())
+# def main():
+#     synonym_recommend = synonym_recommendation(model="llama2", targetWord="CT", sentence=True, cntxt_len=2, text="The radiological evaluation of patients with acute spinal trauma has always been a challenging problem. Multiple radiological procedures are often necessary for \
+#                                                complete evaluation of the extent of spinal injury. CT provides an ideal modality whereby accurate assessment of displacement of bony fragments as well as associated spinal cord and nerve root injury can easily be performed, \
+#                                                eliminating the need for difficult radiological procedures.")
+#     print(synonym_recommend.post_processing())
 # while True:
 #     a = synonym_recommendation(model="llama2", targetWord="BP", sentence=True, cntxt_len=2, text="")
 #     print(a.post_processing())
